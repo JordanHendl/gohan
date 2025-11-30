4121d0c7a5aa16cf09e2ba673df376725904f87d
Jordan Hendl <jordiehendl@gmail.com>
2025-11-30 10:39:15 -0600

    update tare

--- tare/examples/render_graph.rs
+++ tare/examples/render_graph.rs
@@ -1,32 +1,48 @@
+use dashi::*;
+use driver::command::DrawIndexed;
+use tare::graph::*;
 
-fn color_image_attachment() -> ImageInfo {
-
-}
 
 fn main() {
     let mut context = Context::new(&Default::default()).unwrap();
+    let mut graph = RenderGraph::new(&mut context);
 
-    let vertex = ...;
-    let indices = ...;
+    let vertex = graph.make_buffer(&BufferInfo::default());
+    let indices = graph.make_buffer(&BufferInfo::default());
 
-    let graph = RenderGraph::new(context);
-    let target = graph.make_image(color_image_attachment);
-    let subpass_info = {...};
-    let begin_info = {target, ...};
+    let target = graph.make_image(&ImageInfo {
+        debug_name: "[ATTACHMENT]",
+        dim: [1024, 1024, 1],
+        ..Default::default()
+    });
+
+    //let subpass_info = {...};
+    //let begin_info = {target, ...};
     let stream = CommandStream::new();
+    let p_layout = context.make_graphics_pipeline_layout(&GraphicsPipelineLayoutInfo {
+        debug_name: todo!(),
+        vertex_info: todo!(),
+        bg_layouts: todo!(),
+        bt_layouts: todo!(),
+        shaders: todo!(),
+        details: todo!(),
+    }).expect("Make Pipeline Layout"); 
+    
+    let pso = context.make_graphics_pipeline(&GraphicsPipelineInfo::default()).expect("Make graphics pipeline");
+    loop {
+      graph.add_subpass(&SubpassInfo {
+        viewport: todo!(),
+        color_attachments: todo!(),
+        depth_attachment: todo!(),
+        clear_values: todo!(),
+        depth_clear: todo!(),
+    }, |stream| {          
+          let mut s = stream.bind_graphics_pipeline(pso);
 
-    while true {
-      graph.begin_drawing(begin_info, stream);
-      graph.begin_subpass(&subpass_info, |stream| {
-          let pso = graph.build_pso(&PSOInfo {
-            ...
-          });
-          
-          stream.bind_graphics_pipeline(pso.pipeline)
-          stream.draw_indexed(&DrawIndexed{ 
-              vertices: vertex,
-              indices: indices,
-              ..Default::default(),
+          s.draw_indexed(&DrawIndexed{ 
+              vertices: vertex.handle,
+              indices: indices.handle,
+              ..Default::default()
           });
       });
   
--- tare/src/graph/mod.rs
+++ tare/src/graph/mod.rs
@@ -1,4 +1,50 @@
+use std::ptr::NonNull;
+
+use cmd::{Graphics, PendingGraphics};
+use dashi::*;
+
+#[derive(Default, Debug, Clone)]
+pub struct SubpassInfo {
+    pub viewport: Viewport,
+    pub color_attachments: [Option<ImageView>; 8],
+    pub depth_attachment: Option<ImageView>,
+    pub clear_values: [Option<ClearValue>; 8],
+    pub depth_clear: Option<ClearValue>,
+}
 
 pub struct RenderGraph {
+    ctx: NonNull<Context>,
+}
 
+impl RenderGraph {
+    pub fn new(ctx: &mut Context) -> Self {
+        todo!()
+    }
+
+    // Make a transient image matching the parameters input.
+    pub fn make_image(&mut self, info: &ImageInfo) -> ImageView {
+        todo!()
+    }
+
+    // Make a transient buffer matching the parameters input
+    pub fn make_buffer(&mut self, info: &BufferInfo) -> BufferView {
+        todo!()
+    }
+
+    // Append a potential subpass
+    pub fn add_subpass<F>(&mut self, info: &SubpassInfo, mut cb: F)
+    where
+        F: FnMut(CommandStream<PendingGraphics>),
+    {
+        todo!()
+    }
+
+    // All transient data in this object is per-frame. This function advances the frame count to
+    // use next frame's resources.
+    fn advance_frame(&mut self) {}
+
+    pub fn execute(&mut self) {
+        self.advance_frame();
+        todo!()
+    }
 }
